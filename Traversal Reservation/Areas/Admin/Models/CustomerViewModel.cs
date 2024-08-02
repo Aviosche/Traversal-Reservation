144339521494b1c5d3b4d3acdce763b21ab64c7c
Çağatay Batuker <cagatay.batuker@setur.com.tr>
2024-08-02 18:36:04 +0300

    U1: BUGFIX - müşteri silme/güncelleme

--- Traversal Reservation/Areas/Admin/Models/CustomerViewModel.cs
+++ Traversal Reservation/Areas/Admin/Models/CustomerViewModel.cs
@@ -1,12 +1,20 @@
-﻿namespace Traversal_Reservation.Areas.Admin.Models
+﻿using Newtonsoft.Json;
+
+namespace Traversal_Reservation.Areas.Admin.Models
 {
     public class CustomerViewModel
     {
+        [JsonProperty("customerID")]
         public int CustomerID { get; set; }
+        [JsonProperty("name")]
         public string Name { get; set; }
+        [JsonProperty("surname")]
         public string Surname { get; set; }
+        [JsonProperty("city")]
         public string City { get; set; }
+        [JsonProperty("country")]
         public string Country { get; set; }
+        [JsonProperty("mail")]
         public string Mail { get; set; }
 
     }
